766bd20fd70f0e18c757f1995c203f86390b9414
Rajat Raghuvanshi <rajatraghuvanshi1507@gmail.com>
2022-07-07 14:25:25 +0530

    deploying to heroku

--- server/app.js
+++ server/app.js
@@ -24,11 +24,11 @@ app.use("/api", user)
 app.use("/api", notes)
 
 //For production
-// app.use(express.static(path.join(__dirname, "../client/build")))
+app.use(express.static(path.join(__dirname, "../client/build")))
 
-// app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "../client/build/index.html"))
-// })
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client/build/index.html"))
+})
 
 
 app.use(ErrorHandler)
